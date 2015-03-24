94e9b6bca3fee7a667796a19d655b8c12f4f11be
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2015-03-24 19:19:15 +0100

    root for newsarchive re-added

--- dca/tl_news_archive.php
+++ dca/tl_news_archive.php
@@ -10,7 +10,16 @@
 
 $dc = &$GLOBALS['TL_DCA']['tl_news_archive'];
 
+/**
+ * Config
+ */
+$dc['list']['sorting']['fields'] = array('root', 'title');
+
+/**
+ * Palettes
+ */
 $dc['palettes']['default'] = str_replace('title', 'title,displayTitle', $dc['palettes']['default']);
+$dc['palettes']['default'] = str_replace('jumpTo;', 'jumpTo;{root_legend},root;', $dc['palettes']['default']);
 
 $arrFields = array
 (
@@ -23,6 +32,29 @@ $arrFields = array
 		'eval'                    => array('maxlength'=>255),
 		'sql'                     => "varchar(255) NOT NULL default ''"
 	),
+    'root' => array
+    (
+        'label'            => &$GLOBALS['TL_LANG']['tl_news_archive']['root'],
+        'inputType'        => 'select',
+        'options_callback' => array('tl_news_archive_plus', 'getRootPages'),
+        'eval'             => array('includeBlankOption' => true),
+        'sql'              => "int(10) unsigned NOT NULL default '0'",
+    )
 );
 
 $dc['fields'] = array_merge($dc['fields'], $arrFields);
+
+class tl_news_archive_plus extends \Backend
+{
+
+    public function getRootPages(\DataContainer $dc)
+    {
+        $arrOptions = array();
+
+        $objPages = \PageModel::findBy('type', 'root');
+
+        if ($objPages === null) return $arrOptions;
+
+        return $objPages->fetchEach('title');
+    }
+}
--- languages/de/tl_news_archive.php
+++ languages/de/tl_news_archive.php
@@ -8,5 +8,15 @@
  * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
  */
 
+/**
+ * Fields
+ */
 $GLOBALS['TL_LANG']['tl_news_archive']['displayTitle'][0] = 'Anzeigetitle';
 $GLOBALS['TL_LANG']['tl_news_archive']['displayTitle'][1] = 'Bitt geben Sie einen Archive-Anzeigetitel ein.';
+$GLOBALS['TL_LANG']['tl_news_archive']['root'][0] = 'Zugehörigkeit';
+$GLOBALS['TL_LANG']['tl_news_archive']['root'][1] = 'Wählen Sie eine Zugehörigkeit des Archivs zu einem Startpunkt des Seitenbaumes aus.';
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_news_archive']['root_legend'] = 'Zugehörigkeit';

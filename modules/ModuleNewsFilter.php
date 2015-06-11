657d6d21695ab917e5ec25ed807ff511565198fb
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2015-06-11 17:21:29 +0200

    added missing generate method for newsfilter

--- modules/ModuleNewsFilter.php
+++ modules/ModuleNewsFilter.php
@@ -25,6 +25,23 @@ class ModuleNewsFilter extends ModuleNewsPlus
      */
     protected $strCategoryTemplate = 'filter_cat_default';
 
+
+	public function generate()
+	{
+		if (TL_MODE == 'BE') {
+			$objTemplate           = new \BackendTemplate('be_wildcard');
+			$objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD'][$this->type][0]) . ' ###';
+			$objTemplate->title    = $this->headline;
+			$objTemplate->id       = $this->id;
+			$objTemplate->link     = $this->name;
+			$objTemplate->href     = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+
+			return $objTemplate->parse();
+		}
+
+		return parent::generate();
+	}
+
     protected function compile()
     {
         global $objPage;

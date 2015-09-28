7ce04514a99a4448a5f7e76c9e970cd46ca59bcc
Oliver Janke <o.janke@heimrich-hannot.de>
2015-09-28 16:10:41 +0200

    fixed pagination

--- modules/ModuleNewsListPlus.php
+++ modules/ModuleNewsListPlus.php
@@ -106,45 +106,6 @@ class ModuleNewsListPlus extends ModuleNewsPlus
         $this->Template->articles = array();
         $this->Template->empty = $GLOBALS['TL_LANG']['MSC']['emptyList'];
 
-//
-//        // Split the results
-//        if ($this->perPage > 0 && (!isset($limit) || $this->numberOfItems > $this->perPage))
-//		{
-//            // Adjust the overall limit
-//            if (isset($limit)) {
-//                $total = min($limit, $total);
-//            }
-//
-//            // Get the current page
-//            $id = 'page_n' . $this->id;
-//            $page = \Input::get($id) ?: 1;
-//
-//            // Do not index or cache the page if the page number is outside the range
-//            if ($page < 1 || $page > max(ceil($total / $this->perPage), 1)) {
-//                global $objPage;
-//                $objPage->noSearch = 1;
-//                $objPage->cache = 0;
-//
-//                // Send a 404 header
-//                header('HTTP/1.1 404 Not Found');
-//                return;
-//            }
-//
-//            // Set limit and offset
-//            $limit = $this->perPage;
-//            $offset += (max($page, 1) - 1) * $this->perPage;
-//            $skip = intval($this->skipFirst);
-//
-//            // Overall limit
-//            if ($offset + $limit > $total + $skip) {
-//                $limit = $total + $skip - $offset;
-//            }
-//
-//            // Add the pagination menu
-//            $objPagination = new \Pagination($total, $this->perPage, \Config::get('maxPaginationLinks'), $id);
-//            $this->Template->pagination = $objPagination->generate("\n  ");
-//        }
-
 		// Get the total number of items
 		$intTotal = NewsPlusModel::countPublishedByPids($this->news_archives, $this->news_categories, $blnFeatured, array(), $this->startDate, $this->endDate);
 

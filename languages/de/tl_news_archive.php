050d546f437e7beb8a289a9fbeacca5b06254e7d
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2016-03-23 13:22:32 +0100

    translations

--- languages/de/tl_news_archive.php
+++ languages/de/tl_news_archive.php
@@ -23,6 +23,10 @@ $GLOBALS['TL_LANG']['tl_news_archive']['replaceNewsPalette'][0] = 'Nachrichten-P
 $GLOBALS['TL_LANG']['tl_news_archive']['replaceNewsPalette'][1] = 'Die "default"-Palette mit folgender inviduellen Palette überschreiben.';
 $GLOBALS['TL_LANG']['tl_news_archive']['newsPalette'][0] = 'Nachrichten-Palette auswählen';
 $GLOBALS['TL_LANG']['tl_news_archive']['newsPalette'][1] = 'Wählen Sie die Palette aus mit der die "default"-Palette überschrieben werden soll.';
+$GLOBALS['TL_LANG']['tl_news_archive']['limitSubNews'][0] = 'Nachrichtenarchive für Unternachrichten einschränken';
+$GLOBALS['TL_LANG']['tl_news_archive']['limitSubNews'][1] = 'Verfügbare Nachrichtenarchive für Unternachrichten einstellen.';
+$GLOBALS['TL_LANG']['tl_news_archive']['subNewsArchives'][0] = 'Nachrichtenarchive';
+$GLOBALS['TL_LANG']['tl_news_archive']['subNewsArchives'][1] = 'Bitte wählen Sie ein oder mehrere Nachrichtenarchive aus.';
 
 /**
  * Legends
@@ -30,6 +34,7 @@ $GLOBALS['TL_LANG']['tl_news_archive']['newsPalette'][1] = 'Wählen Sie die Pale
 $GLOBALS['TL_LANG']['tl_news_archive']['root_legend'] = 'Zugehörigkeit';
 $GLOBALS['TL_LANG']['tl_news_archive']['image_legend'] = 'Bildkonfiguration';
 $GLOBALS['TL_LANG']['tl_news_archive']['palette_legend'] = 'Paletten-Einstellungen';
+$GLOBALS['TL_LANG']['tl_news_archive']['subnews_legend'] = 'Unternachrichten-Einstellungen';
 
 /**
  * Buttons

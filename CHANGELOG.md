2c77b32c7853ffa69cfed336a562b0da28f1089f
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2016-12-05 17:32:42 +0100

    1.0.39, check changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+## [1.0.39] - 2016-12-05
+
+### Fixed
+- history url/title reworked
--- assets/js/jquery.newsplus.js
+++ assets/js/jquery.newsplus.js
@@ -11,26 +11,25 @@
                 var $modal = $($(this).data('target'));
 
                 // change history base
-                if (!$modal.hasClass('in')) {
-                    $modal.data('history-base-filtered', window.location.href);
-                }
+                $modal.data('history-base', $(this).data('history-base'));
+                $modal.data('history-base-title', $(this).data('history-base-title'));
             });
         },
-        initInfiniteScroll: function(){
+        initInfiniteScroll: function () {
             var arrInfiniteElem = $(document).find('[class^=jscroll_element]'),
                 loadHtml = "<div class='loading'><div class='inside'><div class='spinner'><div class='rect1'></div><div class='rect2'></div><div class='rect3'></div><div class='rect4'></div><div class='rect5'></div></div> Weitere Urteile werden geladen.</div></div>";
 
-            $.each(arrInfiniteElem, function(){
+            $.each(arrInfiniteElem, function () {
                 var infiniteElementSelector = $(this).attr('class'),
                     autoTrigger = $(this).attr('data-autotrigger'),
                     html = '<script>';
-                    html += '$(".'+infiniteElementSelector+'").jscroll({'
-                         +  'debug: false,'
-                         +  'loadingHtml: "'+loadHtml+'",'
-                         +  'nextSelector: ".pagination a.next",'
-                         +  'autoTrigger: '+autoTrigger+','
-                         +  'contentSelector: ".'+infiniteElementSelector+'"});';
-                    html += '</script>';
+                html += '$(".' + infiniteElementSelector + '").jscroll({'
+                    + 'debug: false,'
+                    + 'loadingHtml: "' + loadHtml + '",'
+                    + 'nextSelector: ".pagination a.next",'
+                    + 'autoTrigger: ' + autoTrigger + ','
+                    + 'contentSelector: ".' + infiniteElementSelector + '"});';
+                html += '</script>';
                 $(this).prepend(html);
             });
 
